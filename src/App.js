62f55974974720c15502744b6b7e3c5e2c888aea
guapokat <virgilmartinez@gmail.com>
2018-03-13 23:55:51 -0500

    Added ability to enter image url and receive a response from Clarifai API. Using machine learning it detects human faces and returns a bounding box coordinate system. Also added some stylistic components mainly thorugh Tachyon and CSS. Next step is to properly draw a box on the face that is detected. Then it is on to authentiaction and back-end.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,69 @@
 import React, { Component } from 'react';
-import Navigation from './components/navigation/Navigation'
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import Navigation from './components/navigation/Navigation';
+import FaceRecognition from './components/faceRecognition/FaceRecognition';
+import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
+import Rank from './components/rank/Rank';
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: 'b2732782d09f4e7a8f0b9b5f8ad42953',
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageURL: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  //this.state.import not this.state.inputurl
+  onButtonSubmit = () => {
+    this.setState({imageURL: this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(response) {
+        // do something with response
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+        params={particlesOptions}
+        />
         <Navigation />
-{/*        <Logo />
-        <ImageLinkForm />
-        <FaceRecognition />*/}
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition imageURL={this.state.imageURL}/>
       </div>
-    );
+        );
   }
 }
 
